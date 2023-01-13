bcca7d2048f2993d5010aadb08e244fb0d2d55e6
YassirKarmati <yassirkarmati@gmail.com>
2023-01-13 17:23:40 +0100

    13/01 Structure du front, mailer

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const projectRouter = express.Router();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({ 
+service : 'gmail',
+auth : {
+   user: "fonsat.nodemailer@gmail.com",
+   pass: "dlclhbrybfcawlgi",
+},
+tls: {rejectUnauthorized: false}
+
+})
 
 projectRouter.get("/", async (req, res) => {
   try {
@@ -9,6 +20,20 @@ projectRouter.get("/", async (req, res) => {
     res.send(err);
   }
 });
+
+projectRouter.post("/sendMail", async(req, res) =>{
+  try{
+    await transporter.sendMail({
+      from: req.body.mail, // sender address
+      to: "yassirkarmati@gmail.com", // list of receivers
+      subject: "Prise de contact - Portfolio", // Subject line
+      text: req.body.msg, // html body
+    });
+    res.redirect("/")
+  } catch (err){
+    res.send(err);
+  }
+})
 
 
 
